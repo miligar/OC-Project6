56ef363a1bcf208e2f6ad785c241bc6eef120f57
miligar <emilguiga@gmail.com>
2022-05-09 13:11:24 +0100

    likes added

--- backend/controllers/sauce.js
+++ backend/controllers/sauce.js
@@ -144,3 +144,54 @@ exports.createSauce = (req, res, next) => {
       }
     );
   };
+
+  exports.likeSauce = (req, res, next) => {
+    Sauce.findOne({
+      _id: req.params.id
+    }).then(
+      (sauce) => {
+
+        let userId = req.body.userId;
+        let findLike = sauce.usersLiked.includes(userId);
+        let findDislike = sauce.usersDisliked.includes(userId);
+
+        if (req.body.like ===1&&!findLike&&!findDislike) {
+          console.log('User liking this sauce');
+          sauce.likes++; 
+          sauce.usersLiked.push(userId);
+          console.log(sauce.usersLiked);
+        }
+
+        if (req.body.like ===-1&&!findLike&&!findDislike) {
+          console.log('User disliking this sauce');
+          sauce.dislikes--; 
+          sauce.usersDisliked.push(userId);
+          console.log(sauce.usersDisliked);
+        }
+
+        if (req.body.like ===0&&findLike){
+          console.log('User already liked this sauce and now is neutral');
+          sauce.likes--;
+          sauce.usersLiked = sauce.usersLiked.filter((id) => id!=userId);
+        } else if (req.body.like ===0&&findDislike) {
+          console.log('User already disliked this sauce and now is neutral');
+          sauce.dislikes--;
+          sauce.usersdsliked = sauce.usersLiked.filter((id) => id!=userId);
+        }
+     
+       Sauce.updateOne({_id: req.params.id}, sauce).then(
+       () => {
+        res.status(201).json({
+          message: 'Sauce rating updated successfully!'
+        });
+      }
+       ).catch(
+        (error) => {
+          res.status(400).json({
+            error: error
+          });
+        }
+       );
+      }
+    )
+  }
--- backend/routes/sauce.js
+++ backend/routes/sauce.js
@@ -11,5 +11,6 @@ router.get('/', auth, sauceCtrl.getAllSauces);
 router.get('/:id', auth, sauceCtrl.getOneSauce); 
 router.delete('/:id', auth, sauceCtrl.deleteSauce);
 router.put('/:id', auth, multer, sauceCtrl.modifySauce);
+router.post('/:id/like', auth, sauceCtrl.likeSauce);
 
 module.exports = router;
